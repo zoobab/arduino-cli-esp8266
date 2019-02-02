e1dbed100a85e983333db6711d25eaad41951823
zoobab <zoobab@gmail.com>
2019-02-02 10:18:31 +0000

    Update blink2.ino

--- blink2.ino
+++ blink2.ino
@@ -6,8 +6,8 @@ void setup() {
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
   Serial.print("high\n");
-  delay(2000);
+  delay(5000);
   digitalWrite(LED_BUILTIN, LOW);
   Serial.print("low\n");
-  delay(2000);
+  delay(5000);
 }
