159b05be19f6fad4b5f95cfaf9d237959caf588b
Benjamin Henrion (zoobab) <zoobab@gmail.com>
2019-02-03 00:37:12 +0100

    add serial and time variable

--- blink2.ino
+++ blink2.ino
@@ -1,10 +1,15 @@
+int mydelay = 10000 ;
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
+  Serial.println("HIGH");
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(3000);
+  delay(mydelay);
+  Serial.println("LOW");
   digitalWrite(LED_BUILTIN, LOW);
-  delay(3000);
+  delay(mydelay);
 }
